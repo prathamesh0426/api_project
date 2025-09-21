e32b83439b7528615f5b5795ccbf7713b5b49e82
prathamesh0426 <prathameshj255@gmail.com>
2025-09-21 13:59:41 +0530

    com

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ client = get_stock_client()
 
 # Add the title to webpage
 st.title("Stock Market Project")
-st.subheader("by Utkarsh Gaikwad")
+st.subheader("by Prathamesh Jadhav")
 
 # Take company name as input from user
 company = st.text_input("Please enter company name : ")
